366f0cb6c369c1e8961136b6cd4786842577571a
Micheel10 <masapen3@gmail.com>
2026-08-28 00:46:22 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 
 package GUI;
 
-import practica.pkg1.Practica1;
+import practica.pkg2.Practica2;
 
-public class Main {
-    
+public class Main  {
+
     public static void main(String[] args) {
-        Practica1 practica1 = new Practica1();    
+        Practica2 practica2 = new Practica2();    
     }
 }
